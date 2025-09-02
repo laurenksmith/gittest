c4f6103304e51779ae8196c015e589239fd9024f
Lauren Smith <laurenkimsmith@gmail.com>
2025-09-02 16:09:27 +0100

    Adding my new README, variables_examples and .gitignore to my git repo.

---
+++ variables/README.md
@@ -0,0 +1,7 @@
+# Task: Python variable basics
+
+## * Explain: What is a variable (one sentence)
+A variable is a location that allows the user to store objects and different types of data.
+
+## * Explain: Why called a variable? (one sentence)
+It is called a variable, because the stored object/data can change and isn't fixed.
---
+++ variables/variables_examples.py
@@ -0,0 +1,28 @@
+"""
+Task: Python variable basics
+
+* In your Python learning project, create a folder called 'variables', then create a new file called
+'variables_examples.py' for the following tasks
+* Set/assign a variable
+* Try to set a variable with a number value
+* Try to set a variable with a decimal value
+* Try to set a variable with a string value
+* How is using '==' different?
+* Print the values and data types of the variables you set values for above
+* What does it mean that Python is...
+    * A strongly typed language? Compare to a weakly typed language. Include a code example
+    * A dynamically typed language? Compare to a statically typed language. Include a code example
+* Overwrite the value of one of your variables which stores a number
+* Check the id() of the variable before and after you overwrite the variable with a new number
+* Why does the 'id' of the variable change?
+* Assign one variable to another
+* Start with this code: python x = 10 y = x * Check the id of x and y
+* Explain why the id of x and y are the same
+* What happens if after assigning x = y, I give x a different value? Does the id of y change also?
+* Ask the user for some input and print the input to the screen
+* Get the user's name and print to the screen
+* Improve previous code to: Get name, age and DOB details from a user and print the details to the screen
+* If time, improve previous code to:
+    * Prompt the user and get the input on the same line
+    * Print "Hi <name>" on the one line
+"""
