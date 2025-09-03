a08ca199667e61d894cb9434171c597616f00e3c
Lauren Smith <laurenkimsmith@gmail.com>
2025-09-03 16:33:16 +0100

    Adding my finished collections task.

--- python_tasks_and_projects/collections.py
+++ python_tasks_and_projects/collections.py
@@ -91,7 +91,7 @@ essential_item1 = input("What is an essential item you would take? ")
 essential_item2 = input("What is an essential item you would take? ")
 essential_item3 = input("What is an essential item you would take? ")
 # save the items as a tuple
-essentials_tuple = None  # YOUR CODE GOES HERE INSTEAD OF 'None'
+essentials_tuple = (essential_item1, essential_item2, essential_item3)
 # print the tuple
 print("Here are your items as a tuple:", essentials_tuple)
 print("")
@@ -99,7 +99,7 @@ print("I lied. You can take one more item.")
 essential_item4 = input("What is one more essential item you would take? ")
 # try to add the 4th item to the tuple
 # if you can't add the 4th item, work out how to save the 4th item to the tuple
-# YOUR CODE GOES HERE
+additional_tuple = (essential_item4,)
+final_essentials_tuple = (essentials_tuple + additional_tuple)
 
-print("Here are your items as a tuple (with the 4th item added):", essentials_tuple)
-"""
+print("Here are your items as a tuple (with the 4th item added):", final_essentials_tuple)
