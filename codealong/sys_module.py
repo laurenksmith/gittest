cdab02329e8d59a2f1fc43c9143ef7a173460cba
Lauren Smith <laurenkimsmith@gmail.com>
2025-09-05 11:26:52 +0100

    Adding my latest work from today.

---
+++ codealong/sys_module.py
@@ -0,0 +1,7 @@
+import sys
+import datetime
+
+print(sys.path)   # shows us all the paths that are relevant to Python
+print(sys.version)  # version of Python on my computer
+
+print(datetime.datetime.now())
---
+++ codealong/test_modules.py
@@ -0,0 +1,6 @@
+import random
+
+print(random.random())  # gives you a random number between 0 and 1
+
+print(random.randint(1, 10))  # gives you a random number between 1 and 10
+
